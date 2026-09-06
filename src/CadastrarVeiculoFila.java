7b8e17289f5514943531bc26f4863f7f5989976c
FaelCosme <rafael.cross19@gmail.com>
2026-09-06 17:41:31 -0300

    fix: função remover e consultar funcinando.

--- src/CadastrarVeiculoFila.java
+++ src/CadastrarVeiculoFila.java
@@ -1,41 +1,50 @@
 import java.util.ArrayList;
 
-public class CadastrarVeiculoFila{
+public class CadastrarVeiculoFila {
 
     private ArrayList<Veiculo> veiculos = new ArrayList<>();
 
-    public CadastrarVeiculoFila(){
+    public CadastrarVeiculoFila() {
         veiculos = new ArrayList<>();
     }
 
 
-    public void adicionarVeiculo(String placa){
+    public void adicionarVeiculo(String placa) {
         Veiculo novo = new Veiculo(placa);
         veiculos.add(novo);
         System.out.println("Veiculo adicionado com sucesso!");
     }
 
-    public void removerVeiculo(String placa){
-        Veiculo estacionado = new Veiculo(placa);
-        veiculos.remove(estacionado);
-        System.out.println("Veiculo removido com sucesso!");
+    public void removerVeiculo(String placa) {
+        for (Veiculo v : veiculos) { // Percorre a lista real
+            if (v.getPlaca().equals(placa)) { // Compara a placa de cada um
+                veiculos.remove(v);
+                System.out.println("Veiculo removido com sucesso!");
+                return;
+            }
+        }
+        System.out.println("Veiculo não encontrado!");
 
     }
 
-    public void consultarVeiculo(String placa){
-        Veiculo buscarVeiculo = new Veiculo(placa);
-        veiculos.contains(buscarVeiculo);
-        System.out.println("Veiculo consultado com sucesso!");
+    public void consultarVeiculo(String placa) {
+        for (Veiculo v : veiculos) { // Percorre a lista real
+            if (v.getPlaca().equals(placa)) { // Compara a placa de cada um
+                System.out.println("Veículo encontrado: " + v);
+                return;
+            }
+        }
+        System.out.println("Veículo não encontrado!");
+
     }
 
-    public void listarVeiculos(){
+    public void listarVeiculos() {
 
-        for(Veiculo lista : veiculos){
+        for (Veiculo lista : veiculos) {
             System.out.println(lista);
         }
 
     }
-
 
 
 }
